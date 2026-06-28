9351f004b5244ae7c3195061fcad36df5e0beeac
Layan <Layan2185@gmail.com>
2026-06-28 20:38:36 +0300

    Initial data pipeline project

---
+++ project_pipeline.py
@@ -0,0 +1,54 @@
+#this is how to make a pipeline data 
+
+#1. import all the library
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+#2. connect to the API
+url= "https://jsonplaceholder.typicode.com/users"
+response = requests.get(url)
+data =response.json()
+
+#3.convert json into a DataFrame
+df= pd.DataFrame(data) # now I have a table that in dataFrame
+
+#4. cleaning the data, like for example i only need the name,email and company name columns
+df["company_name"] = df["company"].apply(  #this function because the company namw is inside the address
+    lambda company : company["name"]
+)
+df = df[[
+    "name",
+    "email",
+    "company_name"
+]]
+
+# Keep only companies containing "Group"
+df = df[df["company_name"].str.contains("Group")] #does the string contain group  
+
+#5. connect to PostgreSQL
+engine = create_engine(
+    "postgresql+psycopg://layan:1234@localhost:5432/company"
+)
+
+#6. save the data to the database (Postgresql)
+df.to_sql(
+    "customers", #instead of employees (this is the name of the table in the data base)
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+#7. reading the data from PostgreSQL
+query = "SELECT * FROM customers"
+customers_df= pd.read_sql(query, engine) #reading from database and save it in python as DataFrame
+
+# 8. save it as Parquet
+customers_df.to_parquet(
+    "customers.parquet",
+    index=False
+)
+
+#9. print
+print(customers_df)
+
